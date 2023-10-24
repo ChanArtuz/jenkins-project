df0ae8973793e91f45d7fedb6e8a68fe9e348eb9
ChanArtuz <143057704+ChanArtuz@users.noreply.github.com>
2023-10-24 19:01:18 +0800

    Update TestController.java

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 	
 	@GetMapping("/getSquaredValue/{valueToSquare}")
 	long getSquaredValue(@PathVariable("valueToSquare") Long valueToSquare) {
-		return valueToSquare*valueToSquare+10;
+		return valueToSquare*valueToSquare;
 	}
 }
